30b0dbc4e4e5eab73e42496078bd51fa616b877b
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2026-05-18 10:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-//your JS code here. If required.
-
 const form = document.getElementById("loginForm");
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
-    const checkbox = document.getElementById("checkbox");
-    const existingBtn = document.getElementById("submit");
-    const savedUsername = localStorage.getItem("username");
-    const savedPassword = localStorage.getItem("password");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+const savedUsername = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
 
-    if (savedUsername && savedPassword) {
-      existingBtn.style.display = "block";
-    }
+if (savedUsername && savedPassword) {
+  existingBtn.style.display = "block";
+}
 
-    form.addEventListener("submit", function (e) {
-      e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-      alert(`Logged in as ${username.value}`);
+  alert(`Logged in as ${username.value}`);
 
-      if (checkbox.checked) {
-        localStorage.setItem("username", username.value);
-        localStorage.setItem("password", password.value);
+  if (checkbox.checked) {
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("password", password.value);
 
-        existingBtn.style.display = "block";
-      } else {
-        localStorage.removeItem("username");
-        localStorage.removeItem("password");
+    existingBtn.style.display = "block";
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
 
-        existingBtn.style.display = "none";
-      }
-    });
+    existingBtn.style.display = "none";
+  }
+});
 
-    existingBtn.addEventListener("click", function () {
-      const savedUser = localStorage.getItem("username");
-      alert(`Logged in as ${savedUser}`);
-    });
+existingBtn.addEventListener("click", function () {
+  const savedUser = localStorage.getItem("username");
+
+  alert(`Logged in as ${savedUser}`);
+});
